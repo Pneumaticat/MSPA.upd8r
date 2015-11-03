b48260bc8f1b36f53457c917ddd713860cf2de1a
Kevin Liu <batchfiles99@gmail.com>
2015-11-03 12:03:07 -0500

    Remove more lingering traces of AppSettings

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace MSPA.upd8r.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,18 +20,6 @@ namespace MSPA.upd8r.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("9816")]
-        public int SavedLatestPage {
-            get {
-                return ((int)(this["SavedLatestPage"]));
-            }
-            set {
-                this["SavedLatestPage"] = value;
             }
         }
     }
